a0df891dbb947ce5fd493dbba39b3da51c1a9574
florian <florian.beme@victeon.fr>
2022-05-16 13:39:23 +0200

    exo

---
+++ src/getDisplayName.test.ts
@@ -0,0 +1,25 @@
+import getDisplayName from './getDisplayName'
+
+describe('getDisplayName', ()=> {
+    describe('when no specified city', () => {
+        it('returns question mark and full name', ()=> {
+            expect(getDisplayName('Jane', 'Doe')).toBe('[?] Jane Doe')
+        })
+    })
+    describe('when specified city', () => {
+        it('returns city and full name', ()=> {
+            expect(getDisplayName('John', 'Doe', 'Paris')).toBe('[Paris] John Doe')
+        })
+    })
+    describe('when workstudy curriculum is specified', () => {
+        it('returns city and full name followed by - WnS', ()=> {
+            expect(getDisplayName('John', 'Doe', 'Paris', 'workstudy')).toBe('[Paris] John Doe - WnS')
+        })
+    })
+    describe('when any other curriculum is specified', () => {
+        it('returns city and full name not followed by - WnS', ()=> {
+            expect(getDisplayName('John', 'Doe', 'Paris', 'regular')).toBe('[Paris] John Doe')
+        })
+    })
+})
+
--- src/getDisplayName.ts
+++ src/getDisplayName.ts
@@ -1,5 +1,5 @@
-const getDisplayName = (firstName: string, lastName: string, city: string = '') => {
-    return ''
+const getDisplayName = (firstName: string, lastName: string, city: string = '', curriculum: string = '') => {
+    return `[${city.length === 0 ? '?' : city}] ${firstName} ${lastName}${curriculum === 'workstudy' ? ' - WnS' : ''}`
 }
 
 export default getDisplayName
